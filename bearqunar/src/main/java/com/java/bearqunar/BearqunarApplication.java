99c6ecb55f265056a8a57bfc3c60711ebf37b26b
BBearDr <chenjx0515@163.com>
2019-04-28 15:52:38 +0800

    qunar

---
+++ bearqunar/src/main/java/com/java/bearqunar/BearqunarApplication.java
@@ -0,0 +1,13 @@
+package com.java.bearqunar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BearqunarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BearqunarApplication.class, args);
+    }
+
+}
